1b43e4d24851c333cb5cf20c4ddb01b76c3a1e21
Huy <quach.huy2015@gmail.com>
2023-03-25 22:52:29 +0700

    project question toeic

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 100);
+ 
+});
+
+(function ($) {
+  
+    "use strict";
+  
+      // MENU
+      $('.navbar-collapse a').on('click',function(){
+        $(".navbar-collapse").collapse('hide');
+      });
+      
+      // CUSTOM LINK
+      $('.smoothscroll').click(function(){
+        var el = $(this).attr('href');
+        var elWrapped = $(el);
+        var header_height = $('.navbar').height();
+    
+        scrollToDiv(elWrapped,header_height);
+        return false;
+    
+        function scrollToDiv(element,navheight){
+          var offset = element.offset();
+          var offsetTop = offset.top;
+          var totalScroll = offsetTop-navheight;
+    
+          $('body,html').animate({
+          scrollTop: totalScroll
+          }, 300);
+        }
+      });
+    
+    })(window.jQuery);
+  
+  
+  
